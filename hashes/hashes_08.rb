91c9a84ebc10d28bddf4da675aef1bbf854fa78c
Stephen Gontzes <gontzess@gmail.com>
2020-12-07 10:13:34 -0500

    updated challenge file

--- hashes/hashes_08.rb
+++ hashes/hashes_08.rb
@@ -30,3 +30,24 @@ def anograms(arr_words)
 end
 
 anograms(words)
+
+
+=begin
+# LS Solution
+
+result = {}
+
+words.each do |word|
+  key = word.split('').sort.join
+  if result.has_key?(key)
+    result[key].push(word)
+  else
+    result[key] = [word]
+  end
+end
+
+result.each_value do |v|
+  puts "------"
+  p v
+end
+=end
